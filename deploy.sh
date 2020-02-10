dbf166318fd6f639967cedbc8bdde2217707c204
Taye310 <hi838792022@163.com>
2020-02-10 15:33:20 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:Taye310/UnityWeb.git master:gh-pages
